2ff0fd957ba2216a267753e99bc8563a834522a4
n14011 <n14011@std.it-college.ac.jp>
2014-11-27 12:13:34 +0900

    03

--- Exam.java
+++ Exam.java
@@ -158,7 +158,18 @@ public class Exam {
 	*/
 
 	int[] descSort(int[] array) {
-		return iaDummy;
+         for(int i = 0; i < array.length-1; i++){
+            for(int j = i+1; j < array.length; j++){
+              if(array[j] > array[i]){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+               }
+            }
+         }
+
+           return array;
+
 	}
 
 	/*

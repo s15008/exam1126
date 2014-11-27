65aba1f1e867cebe3ea3c3f1114499db37065cb9
Shino Kayashima <s14003@std.it-college.ac.jp>
2014-11-27 11:43:42 +0900

    9問end

--- Exam.java
+++ Exam.java
@@ -3,151 +3,228 @@ import java.util.Random;
 
 public class Exam {
 
-	int iDummy = 0;
-	char cDummy = 'a';
-	boolean bDummy = false;
-	double dDummy = 0.0d;
-	String sDummy = "";
-	int [] iaDummy = new int [0];
+  int iDummy = 0;
+  char cDummy = 'a';
+  boolean bDummy = false;
+  double dDummy = 0.0d;
+  String sDummy = "";
+  int [] iaDummy = new int [0];
 
-	/*
-	問1 引数 count を受け取り,0からcountまで1づつ増加して表示する
-	メソッド countUp を定義せよ。但しwhileを用いること。
-	*/
-	
-	void countUp(int count) {
- 
-	}
+  /*
+     問1 引数 count を受け取り,0からcountまで1づつ増加して表示する
+     メソッド countUp を定義せよ。但しwhileを用いること。
+     */
 
-	/*
-	問2 引数 count を受け取り,countから0まで1づつ減少して表示する
-	メソッド countDown を定義せよ。但しforを用いること。
-	*/
+  void countUp(int count) {
+    Scanner stdIn = new Scanner(System.in); 
 
-	void countDown(int count) {
+    System.out.print("数値を入力してください:");
+    count = stdIn.nextInt();
 
-	}
+    while(true){
+      for(int i = 0;i > count;i++);
 
-	/*
-	問3 intの引数 x を 受け取り，正のであればtrue,負であればfalseを返す
-	メソッド positive を定義せよ。
-	*/
+      if(i > count) {
+        break;
+      }
+      i += count;
+    }
+    System.out.println(count);
+  }
 
-	boolean positive(int x) {
-		return bDummy;
-	}
+  /*
+     問2 引数 count を受け取り,countから0まで1づつ減少して表示する
+     メソッド countDown を定義せよ。但しforを用いること。
+     */
 
-	/*
-	問4 intの引数 x,y を 受け取り数字が大きい方を返す
-	メソッド max を定義せよ。
-	*/
+  void countDown(int count) {
 
-	int max(int x,int y) {
-		return iDummy;
-	}
+  }
 
-	/*
-	問5 intの引数 x,y を 受け取り合計を返す
-	メソッド sum を定義せよ。
-	*/
+  /*
+     問3 intの引数 x を 受け取り，正のであればtrue,負であればfalseを返す
+     メソッド positive を定義せよ。
+     */
 
-	int sum(int x,int y) {
-		return iDummy;
-	}
+  boolean positive(int x) {
+    Scanner stdIn = new Scanner(System.in);
 
-	/*
-	問6 intの引数 x,y,z を 受け取り doubleで 平均を返す
-	メソッド average を定義せよ。
-	*/
+    System.out.print("数値を入力してください:");
+    x = stdIn.nextInt();
 
-	double average (int x,int y,int z) {
-		return dDummy;
-	}
+    if(x > 0) {
+      return true;
+    }else {
+      return false;
+    }
+  }
 
-	/*
-	問7 intの引数 score を 受け取り 成績を返す メソッド rank を定義せよ。
-	成績は 90以上はA 70以上90未満はB 45以上70未満はC その他はDとする。
-	*/
+  /*
+     問4 intの引数 x,y を 受け取り数字が大きい方を返す
+     メソッド max を定義せよ。
+     */
 
-	char rank (int score) {
-		return cDummy;
-	}
+  int max(int x,int y) {
+    Scanner stdIn = new Scanner(Syste.in);
+    
+    System.out.print("数値を入力してください(1):");
+    x = stdIn.nextInt();
 
-	/*
-	問8 intの引数 width,height を 受け取り 四角形の面積を返す
-	メソッド getRectAreaを定義せよ
-	*/
+    System.out.print("数値を入力してください(2):");
+    y = stdIn.nextInt();
 
-	int getRectArea (int width, int height) {
-		return iDummy;
-	}
+    return Math.abs(x,y);
+  }
 
-	/*
-	問9 三角形の辺の長さ x,y,z を 受け取り 三角形の面積を返す
-	メソッド getTriangleAreaを定義せよ
-	*/
+  /*
+     問5 intの引数 x,y を 受け取り合計を返す
+     メソッド sum を定義せよ。
+     */
 
-	double getTriangleArea (int x,int y,int z) {
-		return dDummy;
-	}
+  int sum(int x,int y) {
+      Scanner stdIn = new Scanner(Systeme.in);
+    
+      System.out.print("値を入力して下さい(1):");
+      x = stdIn.nextInt();
 
-	/*
-	問10 height(cm),weight(kg)を受け取り そのBMI値を返す
-	メソッド calcBMIを定義せよ
-	BMI＝体重（kg）÷（身長（m）×身長（m））
-	*/
+      System.out.print("値を入力して下さい(2):");
+      y = stdIn.nextInt();
 
-	double calcBMI (int x,int y,int z) {
-		return dDummy;
-	}
+      int Sum = (x + y);
+    return Sum;
+  }
 
-	/*
-	問11 最小値 min, 最大値 max を受け取りその範囲のランダムな数を返す
-	メソッド getRandom を定義せよ。
-	*/
+  /*
+     問6 intの引数 x,y,z を 受け取り doubleで 平均を返す
+     メソッド average を定義せよ。
+     */
 
-	int getRandom (int min,int max) {
-		return iDummy;
-	}
+  double average (int x,int y,int z) {
+    Scanner stdIn = new Scanner(System.in);
+    
+    System.out.print("値を入力して下さい(1):");
+    x = stdIn.nextInt();
 
-	/*
-	問12 int型の配列arrayを引数でとり，その配列の要素を降順(大きい順)でソートして返す
-	メソッド descSortを定義せよ
-	*/
+    System.out.print("値を入力して下さい(2):");
+    y = stdIn.nextInt();
 
-	int[] descSort(int[] array) {
-		return iaDummy;
-	}
+    System.out.print("値を入力して下さい(3):");
+    z = stdIn.nextInt();
 
-	/*
-	問13 int型の引数 x を受け取り 
-	3 で割り切れるならばFizz
-	5 で割り切れるなら Buzz
-	3 でも 5 でも割り切れる場合は，FizzBuzz
-	それ以外は x をStringに変換して返す
-	メソッドFizzBuzzを定義せよ
-	*/
+    double Average(x + y + z )/3;
+    return Average;
+  }
 
-	String FizzBuzz(int x) {
-		return sDummy;
-	}
+  /*
+     問7 intの引数 score を 受け取り 成績を返す メソッド rank を定義せよ。
+     成績は 90以上はA 70以上90未満はB 45以上70未満はC その他はDとする。
+     */
 
-	/*
-	問14 掛け算九九表を出力するメソッドTimesTableを作成せよ
-	但し 2次元配列 timestable を用いること。
-	*/
+  char rank (int score) {
+    Scanner stdIn = new Scanner(System.in);
+    
+    System.out.print("点数を入力して下さい:");
+    score = stdIn.nextInt();
 
-	void TimesTable(){
-		int[][] timestable = new int[9][9];
-	}
+    if(score < 45) {
+      if(score > 89) {
+        return A;
+      }else if(score > 69) {
+        return B
+      }else  {
+        return C;
+      }
+    }else {
+      return D;
+    }
+  }
 
-	/*
-	問15 自由課題
-	自由に Java のプログラミングをせよ，但し説明をつけよ
-	*/
+  /*
+     問8 intの引数 width,height を 受け取り 四角形の面積を返す
+     メソッド getRectAreaを定義せよ
+     */
 
-	void execute() {
+  int getRectArea (int width, int height) {
+    return iDummy;
+  }
 
-	}
+  /*
+     問9 三角形の辺の長さ x,y,z を 受け取り 三角形の面積を返す
+     メソッド getTriangleAreaを定義せよ
+     */
+
+  double getTriangleArea (int x,int y,int z) {
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.print("辺xの値を入力して下さい:");
+    x = stdIn.nextInt();
+
+    System.out.print("辺yの値を入力して下さい:");
+    y = stdIn.nextInt();
+
+    System.out.print("辺zの値を入力して下さい:");
+    z = stdIn.nextInt();
+
+    int s = (x + y + z)/2;
+    return "三角形の面積は" + Math.abs(s * (s - x) * (s - y) * (s - z)) + "です。";
+  }
+
+  /*
+     問10 height(cm),weight(kg)を受け取り そのBMI値を返す
+     メソッド calcBMIを定義せよ
+     BMI＝体重（kg）÷（身長（m）×身長（m））
+     */
+
+  double calcBMI (int x,int y,int z) {
+      }
+
+  /*
+     問11 最小値 min, 最大値 max を受け取りその範囲のランダムな数を返す
+     メソッド getRandom を定義せよ。
+     */
+
+  int getRandom (int min,int max) {
+    return iDummy;
+  }
+
+  /*
+     問12 int型の配列arrayを引数でとり，その配列の要素を降順(大きい順)でソートして返す
+     メソッド descSortを定義せよ
+     */
+
+  int[] descSort(int[] array) {
+    return iaDummy;
+  }
+
+  /*
+     問13 int型の引数 x を受け取り 
+     3 で割り切れるならばFizz
+     5 で割り切れるなら Buzz
+     3 でも 5 でも割り切れる場合は，FizzBuzz
+     それ以外は x をStringに変換して返す
+     メソッドFizzBuzzを定義せよ
+     */
+
+  String FizzBuzz(int x) {
+    return sDummy;
+  }
+
+  /*
+     問14 掛け算九九表を出力するメソッドTimesTableを作成せよ
+     但し 2次元配列 timestable を用いること。
+     */
+
+  void TimesTable(){
+    int[][] timestable = new int[9][9];
+  }
+
+  /*
+     問15 自由課題
+     自由に Java のプログラミングをせよ，但し説明をつけよ
+     */
+
+  void execute() {
+
+  }
 
 }

1b37c76b4508951fd458dc3bcdbbaf8262869879
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2015-11-04 11:29:53 +0900

    add rank()

--- Exam.java
+++ Exam.java
@@ -75,7 +75,18 @@ public class Exam {
 	*/
 
 	char rank (int score) {
-		return cDummy;
+        if ( score >= 90) {
+            return 'A';
+        }
+        else if ( score >= 70) {
+            return 'B';
+        }
+        else if ( score >= 45) {
+            return 'C';
+        }
+        else {
+            return 'D';
+        }
 	}
 
 	/*
